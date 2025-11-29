87cbd58abd2a2c973b414858264ac69b8ba559c2
PhilsATM <167253637+PhilsATM@users.noreply.github.com>
2025-11-29 17:09:08 -0300

    feat: simulador de paginacion de memoria virtual
    simple simulador de paginacion de memoria virtual utilizando un algoritmo de reloj clasico (Ejercicio 2)

---
+++ Ejercicio 2/include/Algoritmos.h
@@ -0,0 +1,7 @@
+#ifndef ALGORITMOS_H
+#define ALGORITMOS_H
+
+// declaracion de la funcion
+int clockReplacement(int references[], int n, int frames, int *marcos, int *useBit, int *fallosPorReferencia);
+
+#endif
